546a33cd74200ff06e2ee529bc86a68a136f2945
Miguel Borja Cloud9 <borjainside@gmail.com>
2019-11-30 19:28:40 +0000

    Add linting config

--- echo.js
+++ echo.js
@@ -1,33 +1,34 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express()
-const port = 3000
 
-app.use(bodyParser.urlencoded({extended: false, limit: '10000mb'}));
-app.use(bodyParser.json({limit: '10000mb'}));
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false, limit: '10000mb' }));
+app.use(bodyParser.json({ limit: '10000mb' }));
 app.use(cors());
 
 
 app.post('/echo', (req, res) => {
-	let body = {
-		body: req.body,
-		headers: req.headers
-		}
-	
-	var currentDate = new Date();
-	console.log(currentDate);
-	console.log('Request: ' + JSON.stringify(req.body, null, 4));
-	
-	console.log('Response: ' + JSON.stringify(body, null, 4));
-	res.json(body)
+  const body = {
+    body: req.body,
+    headers: req.headers,
+  };
+
+  const currentDate = new Date();
+  console.log(currentDate);
+  console.log(`Request: ${JSON.stringify(req.body, null, 4)}`);
+
+  console.log(`Response: ${JSON.stringify(body, null, 4)}`);
+  res.json(body);
 });
 
 app.post('/trueecho', (req, res) => {
-	var currentDate = new Date();
-	console.log(currentDate);
-	console.log('Response: ' + JSON.stringify(req.body, null, 4));
-	res.json(req.body)
+  const currentDate = new Date();
+  console.log(currentDate);
+  console.log(`Response: ${JSON.stringify(req.body, null, 4)}`);
+  res.json(req.body);
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
